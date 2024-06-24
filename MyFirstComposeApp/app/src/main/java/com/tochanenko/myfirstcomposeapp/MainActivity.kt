b0945275603d93a164023edfb98e2deacda2209d
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-06-24 19:15:56 +0300

    Init project for Jetpack Compose Tutorial

---
+++ MyFirstComposeApp/app/src/main/java/com/tochanenko/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tochanenko.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tochanenko.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstComposeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstComposeAppTheme {
+        Greeting("Android")
+    }
+}
