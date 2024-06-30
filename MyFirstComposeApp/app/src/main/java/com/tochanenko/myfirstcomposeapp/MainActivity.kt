ebf811d81fc2712248ea6574d1da43bdcfc9d202
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-06-30 18:05:06 +0300

    Modify custom composable

--- MyFirstComposeApp/app/src/main/java/com/tochanenko/myfirstcomposeapp/MainActivity.kt
+++ MyFirstComposeApp/app/src/main/java/com/tochanenko/myfirstcomposeapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.tochanenko.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
@@ -20,10 +23,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Hello(name = "Red Android", color = Color.Red)
+                        Hello(name = "Green Android", color = Color.Green)
+                        Hello(name = "Blue Android", color = Color.Blue)
+                    }
+
                 }
             }
         }
@@ -31,17 +36,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Hello(name: String, modifier: Modifier = Modifier, color: Color = Color.Black) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = color,
+        style = MaterialTheme.typography.headlineMedium
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HelloPreview() {
     MyFirstComposeAppTheme {
-        Greeting("Android")
+        Hello(
+            name = "There",
+            color = Color.Magenta
+        )
     }
 }
